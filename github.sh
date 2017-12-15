367e8b1ba5313c23793ca013f0b886e00b4e1010
stevenjimbus <steven.jimenez.bustamante@gmail.com>
2017-12-15 13:13:56 +0100

    Fecha: 15-dic-13_13

--- github.sh
+++ github.sh
@@ -3,7 +3,8 @@
 echo "Push a Bitbucket"
 git add --all
 git commit -m "Fecha: $(date +%d-%b-%H_%M)"
-git push origin modificacion_rcnn_live_detector 
+git push -u origin master
+ 
 
 echo "Push finalizado"
 
